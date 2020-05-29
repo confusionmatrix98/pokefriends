8a8eb235a80aaf94fbc4cf8bedbcd8154c943d5c
confusionmatrix98 <brandlee1998@gmail.com>
2020-05-29 18:11:58 +0800

    Updated Card image dimensions

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, id}) => {
     return (
         <div className='bg-light-green br3 pa3 ma2 grow bw2 shadow-5 tc'>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt='pokemon' />
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} width='128px' height='128px' alt='pokemon' />
             <div>
                 <h2>{name}</h2>
                 <p>{`#${id}`}</p>
